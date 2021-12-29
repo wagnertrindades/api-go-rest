d4eec95d91810201dd2863a8a7e9483b486d0538
wtrindades <wtrindades@gmail.com>
2021-12-29 19:00:14 -0300

    Create Personalidades endpoint

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/wagnertrindades/go-rest-api/models"
 	"github.com/wagnertrindades/go-rest-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando Servidor Rest com Go")
 	routes.HandleRequest()
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,5 +9,7 @@ import (
 
 func HandleRequest() {
 	http.HandleFunc("/", controllers.Home)
+	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
