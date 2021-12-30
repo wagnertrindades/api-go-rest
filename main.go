fddb0a8ca1fdb5388303430faacfc9a9043e2818
wtrindades <wtrindades@gmail.com>
2021-12-29 21:12:25 -0300

    Connect database

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/wagnertrindades/go-rest-api/database"
 	"github.com/wagnertrindades/go-rest-api/models"
 	"github.com/wagnertrindades/go-rest-api/routes"
 )
@@ -12,6 +13,8 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando Servidor Rest com Go")
 	routes.HandleRequest()
