b818871f863f41f8788b4fdb68713a1650f35887
wtrindades <wtrindades@gmail.com>
2021-12-30 11:00:11 -0300

    Add middleware to change content type to json

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/wagnertrindades/go-rest-api/controllers"
+	"github.com/wagnertrindades/go-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
